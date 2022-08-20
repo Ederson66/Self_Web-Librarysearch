2aa41eb0881b80bb16e526b75b7c3af530706f2c
ederson66 <nduydu66@gmail.com>
2022-08-20 15:11:47 +0700

    ok

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(window).load(function () {
+    var bootstrap = document.querySelector('#bootstrap');
+    var other = document.querySelector('#other');
+    var sql = document.querySelector('#sql');
+    var javascript = document.querySelector('#javascript');
+
+});
