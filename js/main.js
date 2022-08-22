d0999ada4e456ef9cdf96dfb00a43a4174b16d7a
ederson66 <nduydu66@gmail.com>
2022-08-22 14:56:43 +0700

    ok-1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
 $(window).load(function () {
-    var bootstrap = document.querySelector('#bootstrap');
-    var other = document.querySelector('#other');
-    var sql = document.querySelector('#sql');
-    var javascript = document.querySelector('#javascript');
+    // lấy btn 
+    var BtnBootstrap = document.querySelector('#btn-bootstrap');
+    var BtnOther = document.querySelector('#btn-other');
+    var BtnSql = document.querySelector('#btn-sql');
+    var Btnjavascript = document.querySelector('#btn-javascript');
+    var Btnwebserver = document.querySelector('#btn-webserver');
+    // lấy table
+    var BootstrapTbl = document.querySelector('.bootstrap');
+    var OtherTbl = document.querySelector('.other');
+    var SqlTbl = document.querySelector('.sql');
+    var JavascriptTbl = document.querySelector('.javascript');
+    var WebserverTbl = document.querySelector('.webserver');
+
+
+
+    // add sự kiện click
+    BtnBootstrap.addEventListener('click', () => {
+        if (BootstrapTbl.style.display === "block") {
+            BootstrapTbl.style.display = "none";
+        } else {
+            BootstrapTbl.style.display = "block";
+        }
+    });
+
+    BtnOther.addEventListener('click', () => {
+        if (OtherTbl.style.display === "block") {
+            OtherTbl.style.display = "none";
+        } else {
+            OtherTbl.style.display = "block";
+        }
+    });
+
+    BtnSql.addEventListener('click', () => {
+        if (SqlTbl.style.display === "block") {
+            SqlTbl.style.display = "none";
+        } else {
+            SqlTbl.style.display = "block";
+        }
+    });
+
+    Btnjavascript.addEventListener('click', () => {
+        if (JavascriptTbl.style.display === "block") {
+            JavascriptTbl.style.display = "none";
+        } else {
+            JavascriptTbl.style.display = "block";
+        }
+    });
 
 });
